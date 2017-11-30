653e459f1f17b2c0e43af5a0c3a600d73a9077e3
Vytautas <vginteris@gmail.com>
2017-11-30 13:50:09 +0200

    improve test

---
+++ src/test/java/school/controller/ParentControllerTest.java
@@ -0,0 +1,36 @@
+package school.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParentControllerTest {
+    @Test
+    public void getAllParent() throws Exception {
+    }
+
+    @Test
+    public void addNewParent() throws Exception {
+    }
+
+    @Test
+    public void newParentTable() throws Exception {
+    }
+
+    @Test
+    public void updateParent() throws Exception {
+    }
+
+    @Test
+    public void deleteParent() throws Exception {
+    }
+
+    @Test
+    public void getOneParentInformation() throws Exception {
+    }
+
+    @Test
+    public void updateParentInformation() throws Exception {
+    }
+
+}
